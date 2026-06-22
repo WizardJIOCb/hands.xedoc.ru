86374ff8a389b039e212c83681ab0edd5715d35b
Rodion <rodion89@list.ru>
2026-06-22 16:59:18 +0500

    Keep selected mask presets

--- src/generated/maskPresets.ts
+++ src/generated/maskPresets.ts
@@ -6,29 +6,9 @@ export type MaskPreset = {
 
 export const maskPresets = [
   {
-    "id": "chaplin",
-    "label": "Chaplin",
-    "url": "/masks/Chaplin.jpg"
-  },
-  {
-    "id": "churchill",
-    "label": "Churchill",
-    "url": "/masks/Churchill.jpg"
-  },
-  {
-    "id": "dali",
-    "label": "Dali",
-    "url": "/masks/Dali.jpg"
-  },
-  {
     "id": "einstein",
     "label": "Einstein",
     "url": "/masks/Einstein.jpg"
-  },
-  {
-    "id": "elon-musk",
-    "label": "Elon_Musk",
-    "url": "/masks/Elon_Musk.jpg"
   },
   {
     "id": "joker",
@@ -36,19 +16,9 @@ export const maskPresets = [
     "url": "/masks/Joker.jpg"
   },
   {
-    "id": "lincoln",
-    "label": "Lincoln",
-    "url": "/masks/Lincoln.jpg"
-  },
-  {
     "id": "lukashenko",
     "label": "Lukashenko",
     "url": "/masks/Lukashenko.jpg"
-  },
-  {
-    "id": "mona-lisa",
-    "label": "Mona_Lisa",
-    "url": "/masks/Mona_Lisa.jpg"
   },
   {
     "id": "obama",
@@ -56,28 +26,8 @@ export const maskPresets = [
     "url": "/masks/Obama.jpg"
   },
   {
-    "id": "santa-claus",
-    "label": "Santa_Claus",
-    "url": "/masks/Santa_Claus.jpg"
-  },
-  {
-    "id": "sherlock-holmes",
-    "label": "Sherlock_Holmes",
-    "url": "/masks/Sherlock_Holmes.jpg"
-  },
-  {
-    "id": "tesla",
-    "label": "Tesla",
-    "url": "/masks/Tesla.jpg"
-  },
-  {
     "id": "trump",
     "label": "Trump",
     "url": "/masks/Trump.jpg"
-  },
-  {
-    "id": "uncle-sam",
-    "label": "Uncle_Sam",
-    "url": "/masks/Uncle_Sam.jpg"
   }
 ] satisfies MaskPreset[]
